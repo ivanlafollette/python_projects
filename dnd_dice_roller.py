24e4764a4431a7f5ead5fb3611bd3b25757bdcf7
Ivan "Taz" LaFollette <18428543+ivanlafollette@users.noreply.github.com>
2023-07-15 02:21:07 -0400

    Add files via upload

---
+++ dnd_dice_roller.py
@@ -0,0 +1,33 @@
+# Need this for the radomizer. 
+import random
+
+print("-----------------")
+print(" DnD Dice Roller")
+print("-----------------")
+print()
+
+def diceRoller(desiredDice):
+  # Creates the random number plus the number of times being rolled. 
+  for i in range (manyTimes):
+    myNumber = random.randint(1, desiredDice)
+    print("Out of", desiredDice, "you rolled a", myNumber)
+
+while True:
+  choices = [4, 6, 8, 10, 12, 20]
+  print("What sided dice do you want? It must be D4, D6, D8, D10, D12, or D20.")
+  desiredDice = int(input("Number only > "))
+  
+  if desiredDice not in choices:
+        print("Try again. It must be one of the listed sided dice.")
+        continue
+      
+  print("How many times do you want to make this roll? ")
+  manyTimes = int(input(" > "))
+
+  diceRoller(desiredDice)
+  goAgain = input("Do you want to go again? Y\\N ").lower()
+  if goAgain == "y":
+    continue
+  else:
+    print("Bye!")
+    break
