b3aeed4d4b98b5afb2eb0a4a4ca0c7d97ad43553
Ivan "Taz" LaFollette <18428543+ivanlafollette@users.noreply.github.com>
2023-07-22 23:30:51 -0400

    Update dnd_dice_roller.py

--- dnd_dice_roller.py
+++ dnd_dice_roller.py
@@ -7,27 +7,27 @@ print(" DnD Dice Roller ")
 print("-----------------")
 print()
 
-def diceRoller(desiredDice):
+def dice_roller(desired_dice):
   # Creates the random number plus the number of times being rolled. 
   for i in range (manyTimes):
-    myNumber = random.randint(1, desiredDice)
-    print("Out of", desiredDice, "you rolled a", myNumber)
+    my_number = random.randint(1, desired_dice)
+    print("Out of", desiredDice, "you rolled a", my_number)
 
 while True:
   choices = [4, 6, 8, 10, 12, 20]
   print("What sided dice do you want? It must be D4, D6, D8, D10, D12, or D20.")
-  desiredDice = int(input("Number only > "))
+  desired_dice = int(input("Number only > "))
   
-  if desiredDice not in choices:
+  if desired_dice not in choices:
         print("Try again. It must be one of the listed sided dice.")
         continue
       
   print("How many times do you want to make this roll? ")
   manyTimes = int(input(" > "))
 
-  diceRoller(desiredDice)
-  goAgain = input("Do you want to go again? Y\\N ").lower()
-  if goAgain == "y":
+  dice_roller(desired_dice)
+  go_again = input("Do you want to go again? Y\\N ").lower()
+  if go_again == "y":
     continue
   else:
     print("Bye!")
