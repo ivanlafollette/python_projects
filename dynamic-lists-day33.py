84ed7dd99ae31e25851f1ceefe7f750d80bd5b14
Ivan "Taz" LaFollette <18428543+ivanlafollette@users.noreply.github.com>
2023-07-25 23:45:05 -0400

    Update dynamic-lists-day33.py

--- dynamic-lists-day33.py
+++ dynamic-lists-day33.py
@@ -2,14 +2,24 @@
 my_agenda = []
 
 def print_list():
+  
   print() #this is just to add an extra space between items
+  
   for item in my_agenda:
     print(item)
+    
   print() #this is just to add an extra space between items
   
 while True:
-  item = input("What's next on the Agenda?: ")
-
-# Appends to the list via .append(). The variable, "item," is the input variable. 
-  my_agenda.append(item)
+  
+  menu = input("add or remove?: ")
+  
+  if menu == "add":
+    item = input("What's next on the Agenda?: ")
+    my_agenda.append(item)
+    
+  elif menu == "remove":
+    item = input("What do you want to remove?: ")
+    my_agenda.remove(item)
+    
   print_list()
