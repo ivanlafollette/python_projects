1551edb14f8ab4b4b59f3f0d911cc9d6ffbe99fc
Ivan "Taz" LaFollette <18428543+ivanlafollette@users.noreply.github.com>
2023-08-12 04:20:57 -0400

    Add files via upload

---
+++ mississippi_count_v2.py
@@ -0,0 +1,14 @@
+import time
+
+print("We're going to count to five before we play hide and seek! Ok? Ok!")
+print()
+# Write a for loop that counts to five.
+for i in range(6):
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    print(i, "Mississippi")
+    # Body of the loop - use: time.sleep(1)
+    time.sleep(3)
+
+# Write a print function with the final message.
+print("Ready or not, here I come!")
+
