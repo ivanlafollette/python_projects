12c95f4a76ed2af6bb91520b7f50c2324f8ec731
Ivan "Taz" LaFollette <18428543+ivanlafollette@users.noreply.github.com>
2023-08-15 23:56:08 -0400

    Add files via upload

---
+++ largest_number_determinator.py
@@ -0,0 +1,18 @@
+largest_number = -99999999
+counter = 0
+
+number = int(input("Enter a number or type -1 to end program: "))
+
+while number != -1:
+    if number == -1:
+        continue
+    counter += 1
+
+    if number > largest_number:
+        largest_number = number
+    number = int(input("Enter a number or type -1 to end the program: "))
+
+if counter:
+    print("The largest number is", largest_number)
+else:
+    print("You haven't entered any number.")
