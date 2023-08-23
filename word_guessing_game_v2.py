a9961d720be69f3327eddc610a7c1c26d67f2fd4
Ivan "Taz" LaFollette <18428543+ivanlafollette@users.noreply.github.com>
2023-08-22 23:53:57 -0400

    Update word_guessing_game_v2.py

--- word_guessing_game_v2.py
+++ word_guessing_game_v2.py
@@ -44,7 +44,6 @@ def word_game():
               print("The word so far:", guessed_word)
               guess_total += 1
               print("You have guessed", guess_total, "times.")
-            
           #Checks to see if the guessed letter is in P1's word. 
           elif guess in p1_word:
               print("Correct guess!")
@@ -55,12 +54,10 @@ def word_game():
               print("The word so far:", guessed_word)
               guess_total += 1
               print("You have guessed", guess_total, "times.")
-  
           elif guessed_word == p1_word:
             print("Congratulations! Player 1 wins!")
             print("Game over!")
             break
-              
           elif guess not in guessed_word:
               print("Wrong guess!")
               print("The word so far:", guessed_word)
