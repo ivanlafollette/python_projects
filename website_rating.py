57b0700be1c5972ddd5763510b15c25c8ea9c0a2
Ivan "Taz" LaFollette <18428543+ivanlafollette@users.noreply.github.com>
2023-11-14 21:34:19 -0500

    Update website_rating.py

--- website_rating.py
+++ website_rating.py
@@ -1,6 +1,5 @@
 
 # This Python code creates a dictionary called websiteInfo with keys "name," "url," "description," and "rating," each initially set to None. The code then prompts the user to input relevant values for each key using a for loop that iterates over the keys. The input values are assigned to the corresponding keys in the dictionary.
-
 # After gathering user input, the code prints the entered values for each key in the websiteInfo dictionary using another for loop that iterates over the dictionary's items. The output displays the keys and their associated values.
 
 websiteInfo = {"name": None, "url": None, "description": None, "rating": None}
@@ -11,6 +10,13 @@ for name in websiteInfo.keys():
 print()
 for name, value in websiteInfo.items():
   print(f"{name}: {value}")
+
+startOver = input("\nWould you like to start over? (y/n): ")
+if startOver.lower() == "y":
+    websiteInfo.clear()  # Clear the dictionary
+    print("Dictionary cleared. You can start over.")
+else:
+    print("Thank you for using the program!")
 
 # NOTE 1: The .keys() method is used to return a view of all keys in a dictionary.
 # NOTE 2: The .items() method returns a view of all key-value pairs in a dictionary as tuples.
