9b1778c21206e8ba28d45186f149832a897bbdf0
Ivan "Taz" LaFollette <18428543+ivanlafollette@users.noreply.github.com>
2023-11-28 00:16:59 -0500

    Update pig_latin.py

--- pig_latin.py
+++ pig_latin.py
@@ -1,3 +1,6 @@
-print("Give me words, and I will may them into pig latin!")
+print("I will convert English into pig latin!")
 print()
-userWords = input("So, give me a sentence you want translated into pig latin: ")
+userWords = input("What is your word? ").lower()
+converted = userWords[1:]
+converted2 = userWords[0]
+print(f"Here is your pig latin: {converted}{converted2}ay")
