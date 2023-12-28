3352db7e1a7bdbd71a2db2833339e1532bbc8f13
Ivan "Taz" LaFollette <18428543+ivanlafollette@users.noreply.github.com>
2023-12-27 23:54:57 -0500

    Add files via upload

---
+++ tkinter_base.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry("800x600")
+
+root.title("Application v1")
+
+label = tk.Label(root, text="Hello world", font=("arial", 18))
+
+root.mainloop()
+
+# root.geometry("500x500")
+
+
+
+
